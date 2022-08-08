fcdee64e48ed1dd0a32eeff802dd2316282d1447
finpangestu <195410015@students.akakom.ac.id>
2022-08-08 19:22:42 +0700

    add more program on base file

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -1,4 +1,5 @@
 import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
+import 'dart:io';
 
 /// Fungsi [main] akan menampilkan 2 output
 /// Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada library [belajar_dart]
@@ -7,4 +8,44 @@ void main(List<String> arguments) {
   print('Hello Dart! Dart is great.');
   // Testing documentation comment with [].
   print('6 * 7 = ${belajar_dart.calculate()}');
+
+  // Type Data
+  stdout.write('Nama Anda : ');
+  String name = stdin.readLineSync()!;
+  stdout.write('Usia Anda : ');
+  int age = int.parse(stdin.readLineSync()!);
+  print('Halo $name, usia Anda $age tahun');
+
+  // Operator Logika
+  if (2 < 3 && 2 + 4 == 5) {
+    print('Untuk mencetak ini semua kondisi harus benar');
+  } else {
+    print('2 kurang dari 3, tapi 2 + 4 tidak sama dengan 5, maka ini akan tampil');
+  }
+  // Operator Logika
+  if (false || true || false) {
+    print('Ada satu nilai true');
+  } else {
+    print('Jika semuanya false, maka ini akan tampil');
+  }
+
+  //Exception try and catch
+  try {
+    var a = 7;
+    var b = 0;
+    print(a ~/ b);
+  } catch (e) {
+    print('Exception happened: $e');
+  }
+
+  try {
+    var a = 7;
+    var b = 0;
+    print(a ~/ b);
+  } catch(e, s) {
+    print('Exception happened: $e');
+    print('Stack trace: $s');
+  } finally {
+    print('This line still executed');
+  }
 }
